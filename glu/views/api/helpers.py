6e3332d02a499b08ae4c2b65fb0c2c1fd9adcffc
Nathan Faubion <nathan@n-son.com>
2012-05-24 00:36:45 -0400

    Broken netflix call

--- glu/views/api/helpers.py
+++ glu/views/api/helpers.py
@@ -1,4 +1,4 @@
-from flask import request, session
+from flask import request, session, current_app
 from flask import json, jsonify as _jsonify
 from glu import cache
 from werkzeug.exceptions import HTTPException
@@ -6,6 +6,11 @@ from werkzeug.exceptions import HTTPException
 import functools
 import hashlib
 import time
+import random
+import string
+import urllib
+import httplib
+import json
 
 
 ########################################
@@ -75,3 +80,35 @@ def session_contains(*members):
         return flow_api.mock_response(None, 403, None, errors)
     return apply
   return decorator
+
+
+########################################
+# Netflix
+########################################
+
+def netflix(endpoint, params=None, method='GET'):
+  url = 'http://api.netflix.com' + endpoint
+  params.update({
+    'oauth_consumer_key': current_app.config['NETFLIX_KEY'],
+    'oauth_nonce': ''.join([random.choice(string.letters + string.digits) for x in xrange(20)]),
+    'oauth_signature_method': 'HMAC-SHA1',
+    'timestamp': int(time.time() * 1000),
+    'output': 'json',
+    })
+
+  sig = hashlib.sha1()
+  sig.update(current_app.config['NETFLIX_KEY'])
+  sig.update(current_app.config['NETFLIX_SECRET'])
+  sig.update('%s&%s&%s' % (
+    method,
+    urllib.quote_plus(url),
+    urllib.quote_plus(urllib.urlencode(sorted(params.items(), key=lambda x: x[0])))
+    ))
+
+  params['oauth_signature'] = sig.hexdigest()
+
+  conn = httplib.HTTPConnection('api.netflix.com')
+  conn.request(method, '%s?%s' % (endpoint, urllib.urlencode(params)))
+  resp = conn.getresponse().read()
+
+  return json.loads(resp)
